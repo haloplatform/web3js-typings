e2ae893d6fc540aeaa29248a02e14f479f07154c
shadowcodex <jsduncan98@gmail.com>
2017-11-02 17:18:42 -0500

    top file comments

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for Web3.js v1.0.0-beta.18
+// Project: https://github.com/ethereum/web3.js
+// Definitions by: Shannon Duncan <https://github.com/shadowcodex>, Halo Platform <https://github.com/haloplatform>
+// Definitions: https://github.com/haloplatform/web3js-typings
+
+/************************************************
+*                                               *
+*          Web3.js v1.0.0-beta.18 API           *
+*                                               *
+************************************************/
