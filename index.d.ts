d4a48905e2db9feb851a68f61dcfb1fff33453c3
shadowcodex <jsduncan98@gmail.com>
2017-11-02 18:03:57 -0500

    added version api and started conversion functions

--- index.d.ts
+++ index.d.ts
@@ -8,3 +8,52 @@
 *          Web3.js v1.0.0-beta.18 API           *
 *                                               *
 ************************************************/
+
+
+declare namespace version {
+    const api: string;
+    const node: string;
+    const network: string;
+    const ethereum: string;
+    const whisper: string;
+}
+
+declare function isConnected(): boolean;
+
+// declare function setProvider(new web3.providers.HttpProvider('http://localhost:8545')): object;
+
+declare const currentProvider: object;
+
+declare function reset(keepIsSyncing: boolean): undefined;
+
+declare function sha3(value: string, options?:any): string;
+
+declare function toHex(value: string): string;
+declare function toHex(value: number): string;
+declare function toHex(value: object): string;
+declare function toHex(value: any[]): string;
+declare function toHex(value: any): string; //catch bignumber
+
+declare function toAscii(hexString: string): string;
+
+declare function fromAscii(asciiString: string, padding?:number): string;
+
+declare function toDecimal(hexString: string): number;
+
+declare function fromDecimal(number: number): string;
+declare function fromDecimal(number: string): string;
+
+// Units for fromWei
+// kwei/ada
+// mwei/babbage
+// gwei/shannon
+// szabo
+// finney
+// ether
+// kether/grand/einstein
+// mether
+// gether
+// tether
+declare function fromWei(number: number, unit: string): string;
+declare function fromWei(number: string, unit: string): string;
+declare function fromWei(number: any, unit: string): any;
